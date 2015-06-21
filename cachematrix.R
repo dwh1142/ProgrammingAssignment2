0f47c0e804ac2738c608beb12e447ccb029903b7
dwh1142 <dwh1142@gmail.com>
2015-06-20 23:35:31 -0400

    CacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,8 @@
 #This function creates a "matrix", which is actually a list of functions that do the following:
 # 1. set the value of the matrix
 # 2. get the value of the matrix
-#	3. set the value of the inverse of the matrix
-#	4. get the value of the inverse of the matrix
+# 3. set the value of the inverse of the matrix
+# 4. get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x=matrix()) {
   i <- NULL
