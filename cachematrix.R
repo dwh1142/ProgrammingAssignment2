7760a6344d5b06cbaa54b6eea4f03adc11aa5bd8
dwh1142 <dwh1142@gmail.com>
2015-06-20 23:28:01 -0400

    CacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
-#These functions, in conjunction, find the inverse of a matrix, and store it in cache.  If the inverse of the matrix is already stored in cache, the value is pulled from cache rather than being computed again. 
+#These functions, in conjunction, find the inverse of a matrix, and store it in cache.  
+#If the inverse of the matrix is already stored in cache, the value is pulled from cache rather than being computed again. 
 
 #This function creates a "matrix", which is actually a list of functions that do the following:
-#  1. set the value of the matrix
-#	2. get the value of the matrix
+# 1. set the value of the matrix
+# 2. get the value of the matrix
 #	3. set the value of the inverse of the matrix
 #	4. get the value of the inverse of the matrix
 
@@ -20,7 +21,11 @@ makeCacheMatrix <- function(x=matrix()) {
        getinv = getinv)
 }
 
-#This function calculates the inverse of the "matrix" from the previous function. However, it first checks to see if the inverse has already been calculated.  If so, it returns the inverse stored in cache, rather than computing it again, and prints a message letting the user know this happened. If not, it returns the computed value, and sets the value of the inverse in cache to the computed value.
+#This function calculates the inverse of the "matrix" from the previous function. 
+#However, it first checks to see if the inverse has already been calculated.  
+#If so, it returns the inverse stored in cache, rather than computing it again, 
+#and prints a message letting the user know this happened. 
+#If not, it returns the computed value, and sets the value of the inverse in cache to the computed value.
 
 cacheSolve <- function(x, ...) {
   i <- x$getinv()
